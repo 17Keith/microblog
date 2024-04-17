f5e4edaee6413464b5d63ea06da653d942e67a96
17Keith <kayomega7@gmail.com>
2024-04-18 01:18:10 +0300

    Set up login functionality and template for the app

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 # Use render_template() function
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Keith'}
-    posts= [
+    posts = [
         {
             'author': {'username': 'John'},
             'body': 'Today is going to be a beautiful day!'
@@ -18,3 +20,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+# New Login Route for the app!
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
