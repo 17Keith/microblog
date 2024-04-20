08aacb59d9ed7d4c67f2154cd600ed692924e8f7
17Keith <kayomega7@gmail.com>
2024-04-20 13:11:05 +0300

    Flask-SQLAlchemy and Flask-Migrate initialization

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-import os 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wrong-guess'
+
+#  Flask-SQLAlchemy configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
