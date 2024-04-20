68dd2202d96a0a2a86affb12fc8e1793b9df7a05
17Keith <kayomega7@gmail.com>
2024-04-20 17:46:31 +0300

    Interacting with the db

--- app/models.py
+++ app/models.py
@@ -7,13 +7,14 @@ from app import db
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(
-        sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(
-        sa.String(120), index=True, unique=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    post: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,9 +24,10 @@ class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.gmt))
-    user_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey(User.id), index=True)
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
     author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self):
