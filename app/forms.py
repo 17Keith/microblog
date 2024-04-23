082ff86893ce663aa59fae0d63db9dac3a61d9f9
17Keith <kayomega7@gmail.com>
2024-04-24 01:20:43 +0300

    Allow users to change and edit their  profiles

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,8 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from wtforms import TextAreaField
+from wtforms.validators import Length
 
 
 class LoginForm(FlaskForm):
@@ -39,3 +41,9 @@ class RegistrationForm(FlaskForm):
 
         if user is not None:
             raise ValidationError('Please user a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ import sqlalchemy as sa
 from app import app, db
 from app.models import User
 from datetime import datetime, timezone
+from app.forms import EditProfileForm
 
 
 @app.before_request
@@ -89,3 +90,20 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Great! Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.date = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template('edit_profile.html', title = 'Edit Profile', form=form)
