971531a14a81453d1e88b640bd83552b013bd95d
17Keith <kayomega7@gmail.com>
2024-04-23 15:21:36 +0300

    Set the user profile view and template

--- app/routes.py
+++ app/routes.py
@@ -70,3 +70,14 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
