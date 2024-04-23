e0a6ec08554812b530aaf6ef2ad85012b84e77f1
17Keith <kayomega7@gmail.com>
2024-04-23 19:37:51 +0300

    Add new fields in user model and show user information on profile template

--- app/models.py
+++ app/models.py
@@ -19,6 +19,8 @@ class User(UserMixin, db.Model):
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
 
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.eat))
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
