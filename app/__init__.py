b7e684af38cd417dd368ea5433d2b69d1c259f0a
17Keith <kayomega7@gmail.com>
2024-04-26 18:37:46 +0300

    Fix errors

--- app/__init__.py
+++ app/__init__.py
@@ -30,17 +30,16 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-if not os.path.exists('logs'):
-    os.mkdir('logs')
-file_handler = RotatingFileHandler(
-    'logs/microblog.log', maxbytes=10240, backupCount=10)
-file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [%(pathname)s:%(lineno)d]'
-))
-file_handler.setLevel(logging.INFO)
-file_handler.addHandler(file_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
-app.logger.setLevel(logging.INFO)
-app.logger.info('Microblog startup')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
