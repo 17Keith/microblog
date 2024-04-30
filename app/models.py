b7572c97a66cda85bb51871c4cac3b0ade6f7abc
17Keith <kayomega7@gmail.com>
2024-04-30 10:50:07 +0300

    Followed posts query with user's own posts.

--- app/models.py
+++ app/models.py
@@ -80,6 +80,21 @@ class User(UserMixin, db.Model):
             self.following.select().subquery()
         )
         return db.session.scalar(query)
+    
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower), isouter=True)
+            .where(sa.or_(
+                Follower.id == self.id,
+                Author.id == self.id,
+                ))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())
+        )
 
 
 @login.user_loader
