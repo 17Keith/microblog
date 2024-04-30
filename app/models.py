bd1eb97031b8b8a8333f7c120288f9017d86cfb2
17Keith <kayomega7@gmail.com>
2024-04-30 10:40:08 +0300

    functionality to add and remove followers

--- app/models.py
+++ app/models.py
@@ -57,6 +57,30 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf_8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery()
+        )
+        return db.session.scalar(query)
+    
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery()
+        )
+        return db.session.scalar(query)
+
 
 @login.user_loader
 def load_user(id):
